4492a625dea3f9b23e613bb6709761bb899a179a
Tiago Dinis <tiagoddinis@gmail.com>
2021-10-11 16:17:55 +0100

    deploy fixes

--- pages/index.js
+++ pages/index.js
@@ -9,18 +9,18 @@ import {
   LeftSide,
   RightSide,
   BorderGradient,
-} from "./styles";
+} from "../styles";
 
 export default function Home() {
   return (
     <HomepageContainer>
       <LeftSide>
         <header>
-          <h1>Hi! I'm Tiago Dinis</h1>
+          <h1>Hi! I&apos;m Tiago Dinis</h1>
           <p>Frontend developer focusing on UX, interactivity and data viz.</p>
           <p>
-            I'm currently looking for a job so feel free to 👋 if you like what
-            you see (here's my resume)
+            I&apos;m currently looking for a job so feel free to 👋 if you like
+            what you see (here&apos;s my resume)
           </p>
         </header>
         <Footer>© 2021 Tiago Dinis</Footer>
@@ -29,7 +29,7 @@ export default function Home() {
         <BorderGradient />
         <ProjectList>
           {getProjectMetaData().map((p) => (
-            <Link key={p.slug} href={`/projects/${p.slug}`}>
+            <Link key={p.slug} href={`/projects/${p.slug}`} passHref>
               <ProjectStub>
                 <h3>{p.name}</h3>
                 <div>{p.description}</div>
