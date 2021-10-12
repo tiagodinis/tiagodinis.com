0ccc98a2e035b0953335b60fa50565b0ed1c4fd7
Tiago Dinis <tiagoddinis@gmail.com>
2021-10-12 20:31:41 +0100

    Mobile header layout

--- pages/index.js
+++ pages/index.js
@@ -2,43 +2,105 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import {
-  Footer,
+  // Footer,
+  // HomepageContainer,
+  // ProjectStub,
+  // ProjectList,
+  // LeftSide,
+  // RightSide,
+  // BorderGradient,
   HomepageContainer,
-  ProjectStub,
-  ProjectList,
-  LeftSide,
-  RightSide,
+  Header,
+  Hero,
+  About,
+  TextLink,
+  Social,
+  ProjectsContainer,
   BorderGradient,
+  ProjectList,
+  ProjectStub,
+  Footer,
 } from "../styles";
 
+import GithubSVG from "../components/svg/GithubSVG";
+import TwitterSVG from "../components/svg/TwitterSVG";
+import LinkedInSVG from "../components/svg/LinkedInSVG";
+import MailSVG from "../components/svg/MailSVG";
+
 export default function Home() {
+  const iconSize = 24;
+  const iconIdleColor = "#a2a2a2";
+
   return (
+    // <HomepageContainer>
+    //   <LeftSide>
+    //     <header>
+    //       <h1>Hi! I&apos;m Tiago Dinis</h1>
+    //       <p>Frontend developer focusing on UX, interactivity and data viz.</p>
+    //       <p>
+    //         I&apos;m currently looking for a job so feel free to 👋 if you like
+    //         what you see (here&apos;s my resume)
+    //       </p>
+    //     </header>
+    //     <Footer>© 2021 Tiago Dinis</Footer>
+    //   </LeftSide>
+    //   <RightSide>
+    //     <BorderGradient />
+    //     <ProjectList>
+    //       {getProjectMetaData().map((p) => (
+    //         <ProjectStub key={p.name} onClick={() => window.open(p.slug)}>
+    //           <h3>{p.name}</h3>
+    //           <div>{p.description}</div>
+    //         </ProjectStub>
+    //       ))}
+    //     </ProjectList>
+    //     <BorderGradient bottom />
+    //   </RightSide>
+    // </HomepageContainer>
+
     <HomepageContainer>
-      <LeftSide>
-        <header>
-          <h1>Hi! I&apos;m Tiago Dinis</h1>
-          <p>Frontend developer focusing on UX, interactivity and data viz.</p>
-          <p>
-            I&apos;m currently looking for a job so feel free to 👋 if you like
-            what you see (here&apos;s my resume)
-          </p>
-        </header>
-        <Footer>© 2021 Tiago Dinis</Footer>
-      </LeftSide>
-      <RightSide>
-        <BorderGradient />
-        <ProjectList>
-          {getProjectMetaData().map((p) => (
-            <Link key={p.slug} href={`/projects/${p.slug}`} passHref>
-              <ProjectStub>
-                <h3>{p.name}</h3>
-                <div>{p.description}</div>
-              </ProjectStub>
-            </Link>
-          ))}
-        </ProjectList>
-        <BorderGradient bottom />
-      </RightSide>
+      <Header>
+        <Hero>
+          Making.
+          <br />
+          Breaking.
+          <br />
+          Looking up.
+        </Hero>
+
+        <About>
+          I'm{" "}
+          <TextLink href="https://www.google.com/" target="_blank">
+            Tiago Dinis
+          </TextLink>
+          . Frontend developer focused on UX and interactivity.
+          <br />
+          <br />
+          <TextLink href="https://www.google.com/" target="_blank">
+            Explore
+          </TextLink>{" "}
+          highlighted works, and feel free to{" "}
+          <TextLink href="https://www.google.com/" target="_blank">
+            say hello!
+          </TextLink>
+        </About>
+        <Social>
+          <GithubSVG dim={iconSize} />
+          {/* <MailSVG color={iconIdleColor} dim={38} /> */}
+          <MailSVG dim={28} />
+          <TwitterSVG dim={iconSize} />
+          <LinkedInSVG dim={iconSize} />
+        </Social>
+      </Header>
+      {/* <ProjectsContainer>
+        {getProjectMetaData().map((p) => (
+          <ProjectStub key={p.name} onClick={() => window.open(p.slug)}>
+            <h3>{p.name}</h3>
+            <div>{p.description}</div>
+          </ProjectStub>
+        ))}
+      </ProjectsContainer>
+      <Footer>© 2021 Tiago Dinis</Footer> */}
     </HomepageContainer>
   );
 }
@@ -48,38 +110,42 @@ function getProjectMetaData() {
     {
       name: "Notion Flashcards",
       description: "This is project A",
-      slug: "notion-flashcards",
+      slug: "https://github.com/tiagodinis/notion-flashcards",
     },
     {
       name: "Pocket Calculator",
       description: "This is project A",
-      slug: "pocket-calculator",
+      slug: "https://github.com/tiagodinis/calculator",
     },
-    {
-      name: "Code gallery",
-      description: "This is project C",
-      slug: "code-gallery",
-    },
+    // {
+    //   name: "Code gallery",
+    //   description: "This is project C",
+    //   slug: "https://insofrido.com",
+    // },
     {
       name: "Insofrido.com",
       description: "This is project C",
-      slug: "insofrido",
+      slug: "https://insofrido.com",
     },
     {
       name: "Kendo Warriors",
       description: "This is project A",
-      slug: "kendo-warriors",
+      slug: "https://insofrido.com",
     },
-    { name: "Deltanaut", description: "This is project A", slug: "deltanaut" },
+    {
+      name: "Deltanaut",
+      description: "This is project A",
+      slug: "https://youtu.be/Hdfng36DgX0",
+    },
     {
       name: "GParticles",
       description: "This is project B",
-      slug: "gparticles",
+      slug: "https://github.com/tiagodinis/GParticles",
     },
     {
       name: "Telewarper Staff",
       description: "This is project B",
-      slug: "telewarper-staff",
+      slug: "https://github.com/tiagodinis/TelewarperStaff",
     },
   ];
 }
--- styles/index.js
+++ styles/index.js
@@ -1,59 +1,174 @@
 import styled from "styled-components";
 
+// export const HomepageContainer = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+// `;
+
+// export const LeftSide = styled.div`
+//   width: 100vw;
+//   height: 100vh;
+
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+// `;
+
+// export const RightSide = styled.div`
+//   width: 100vw;
+//   height: 100vh;
+
+//   overflow-y: scroll;
+//   scrollbar-width: none;
+//   ::-webkit-scrollbar {
+//     display: none;
+//   }
+// `;
+
+// export const BorderGradient = styled.div`
+//   height: 200px;
+//   width: 50%;
+
+//   position: absolute;
+//   bottom: ${({ bottom }) => (bottom ? "0px" : "none")};
+
+//   background: linear-gradient(
+//     ${({ bottom }) => (bottom ? "0deg" : "180deg")},
+//     rgba(255, 255, 255, 1) 10%,
+//     rgba(255, 255, 255, 0) 100%
+//   );
+
+//   pointer-events: none;
+// `;
+
+// export const ProjectList = styled.main`
+//   padding: 24vh 0px;
+
+//   display: flex;
+//   flex-direction: column;
+// `;
+
+// export const ProjectStub = styled.main`
+//   border: 1px solid black;
+
+//   padding: 10px;
+//   margin: 20px;
+
+//   cursor: pointer;
+// `;
+
+// export const Footer = styled.footer`
+//   position: absolute;
+//   bottom: 10px;
+// `;
+
+// MOBILE
+
 export const HomepageContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-export const LeftSide = styled.div`
-  /* border: 1px solid black; */
-
   width: 100vw;
   height: 100vh;
 
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  background-color: #fafafa;
+
+  /* display: flex;
+  flex-direction: column;
+  align-items: center; */
 `;
 
-export const RightSide = styled.div`
+export const Header = styled.header`
+  /* border: 1px solid black; */
+  position: relative;
+  left: 48px;
+  top: 56px;
+
+  width: fit-content;
+`;
+
+export const Hero = styled.h1`
   /* border: 1px solid black; */
 
-  width: 100vw;
-  height: 100vh;
+  line-height: 2.7rem;
+  font-family: "Raleway", sans-serif;
+  font-weight: 800;
+  font-size: 34px;
+  color: #4a4a4a;
+`;
 
-  overflow-y: scroll;
-  scrollbar-width: none;
-  ::-webkit-scrollbar {
-    display: none;
+export const About = styled.section`
+  /* border: 1px solid black; */
+
+  margin-top: 40px;
+
+  line-height: 1.6rem;
+  font-family: "Work Sans", sans-serif;
+  font-weight: 300;
+  font-size: 18px;
+  color: #2d2d2d;
+
+  width: 230px;
+`;
+
+export const TextLink = styled.a`
+  font-weight: 500;
+  color: #3b80fe;
+  text-decoration: none;
+
+  &:hover {
+    color: #7ba9fc;
+  }
+
+  transition: color 0.2s ease-out;
+`;
+
+export const Social = styled.nav`
+  width: fit-content;
+  margin-top: 50px;
+
+  display: flex;
+  justify-content: flex-start;
+
+  svg {
+    margin-right: 24px;
+    cursor: pointer;
+    fill: #a2a2a2;
+    transition: fill 0.4s ease-out;
+  }
+
+  svg:nth-child(2) {
+    position: relative;
+    bottom: 1px;
+  }
+
+  svg:nth-child(3) {
+    position: relative;
+    bottom: -2px;
+    left: 1px;
+  }
+
+  svg:last-child {
+    margin-right: 0px;
+  }
+
+  svg:nth-child(1):hover {
+    fill: #37474f;
+  }
+
+  svg:nth-child(2):hover {
+    fill: #5c6bc0;
+  }
+
+  svg:nth-child(3):hover {
+    fill: #03a9f4;
+  }
+
+  svg:nth-child(4):hover {
+    fill: #1976d2;
   }
 `;
 
-export const BorderGradient = styled.div`
-  height: 200px;
-  width: 50%;
-
-  position: absolute;
-  /* top: ${({ bottom }) => (bottom ? "none" : "10vh")}; */
-  bottom: ${({ bottom }) => (bottom ? "0px" : "none")};
-
-  background: linear-gradient(
-    ${({ bottom }) => (bottom ? "0deg" : "180deg")},
-    rgba(255, 255, 255, 1) 10%,
-    rgba(255, 255, 255, 0) 100%
-  );
-
-  pointer-events: none;
-`;
-
-export const ProjectList = styled.main`
+export const ProjectsContainer = styled.div`
   /* border: 1px solid black; */
-
-  padding: 24vh 0px;
-
-  display: flex;
-  flex-direction: column;
 `;
 
 export const ProjectStub = styled.main`
@@ -66,6 +181,7 @@ export const ProjectStub = styled.main`
 `;
 
 export const Footer = styled.footer`
-  position: absolute;
-  bottom: 10px;
+  /* border: 1px solid black; */
+  width: fit-content;
+  margin: auto;
 `;
